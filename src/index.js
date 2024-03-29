1056bc99798823aac9f3289d2637a43bcc9f3205
Suhail TH <suhailth17756@gmail.com>
2024-03-29 22:39:58 +0530

    code resturucured

--- src/index.js
+++ src/index.js
@@ -10,4 +10,5 @@ const store=createStore(reducers,compose(applyMiddleware(thunk)))
 ReactDom.render(
     <Provider store={store}>
         <App/>
+        
     </Provider>,document.getElementById('root'))
