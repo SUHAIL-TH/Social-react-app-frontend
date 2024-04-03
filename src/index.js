ebe7473a7741ecd87208ef09575ee343fcdf76bf
Suhail TH <suhailth17756@gmail.com>
2024-04-03 22:40:57 +0530

    chages added

--- src/index.js
+++ src/index.js
@@ -10,5 +10,4 @@ const store=createStore(reducers,compose(applyMiddleware(thunk)))
 ReactDom.render(
     <Provider store={store}>
         <App/>
-        
     </Provider>,document.getElementById('root'))
